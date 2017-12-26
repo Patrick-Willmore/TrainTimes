58e6cae0dbb40d4df51188b5eb5c1fa13c73b0fc
Patrick Willmore <patman1127@gmail.com>
2017-12-25 21:28:53 -0700

    working

--- logic.js
+++ logic.js
@@ -10,26 +10,52 @@ var config = {
   firebase.initializeApp(config);
   
   var database = firebase.database();
+  
+
+  var firstTime;
+  
 
   $("#addTrain").submit(function(event){
     event.preventDefault();
 
     var trainName = $("#train-name-input").val().trim();
     var dest = $("#train-dest-input").val().trim();
-    var firstTrain = moment($("#train-time-input").val().trim(), "hh:mm").format("X");
+    // var firstTrain = moment($("#train-time-input").val().trim(), "hh:mm").format("X");
+    firstTime = $("#train-time-input").val().trim();
     var freq = $("#train-freq-input").val().trim();
 
+    var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "years");
+    console.log(firstTimeConverted);
+
+    var currentTime = moment(currentTime).format("hh:mm");
+    console.log("current time" + currentTime);
+    // var currentTime = moment();
+    // console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("difference in time" + diffTime);
+    // var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    // console.log("DIFFERENCE IN TIME: " + diffTime);
+    var tRemainder = diffTime % freq;
+    // var tRemainder = diffTime % tFrequency;
+    console.log(tRemainder);
+    // console.log(tRemainder);
+    var minutesTilTrain = freq - tRemainder;
+    console.log("minutes til train" + minutesTilTrain);
+
+    var tilNextTrain = moment().add(minutesTilTrain, "minutes");
+    console.log("til next train: " + tilNextTrain);
+
+    var nextArrival = moment().add(minutesTilTrain, "minutes");
+    console.log(moment(nextArrival).format("hh:mm"));
     var newTrain = {
         name: trainName,
         destination: dest,
-        firstTrain: firstTrain,
+        firstTrain: firstTime,
         frequency: freq
     };
     
     database.ref().push(newTrain);
-
-    console.log(newTrain.name);
-    console.log(newTrain.destination);
-    console.log(newTrain.firstTrain);
-    console.log(newTrain.frequency);
+    $("#trainTable > tbody").append(`<tr><td>${trainName}</td><td>${dest}</td><td>${freq} </td><td>${moment(nextArrival).format("hh:mm")}</td><td>${minutesTilTrain}</td></tr>`);
+  $("#addTrain")[0].reset();
   });
